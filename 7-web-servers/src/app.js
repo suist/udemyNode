768d8d7ac1951fa9b12040beb6db1cd9e56fe154
suist <skoco73@gmail.com>
2020-01-11 21:18:45 +0900

    git ignore add

--- 7-web-servers/src/app.js
+++ 7-web-servers/src/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
-
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 console.log(__dirname)
 console.log(__filename)
@@ -51,6 +52,42 @@ app.get('/about',(req,res)=>{
     })
 })
 
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address) {
+        return res.send({
+            error:' You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
+
+        // const address= req.query.address
+
+
+        // res.send({
+        //     location:
+        // })
+    }
+)
+
 app.get('*',(req,res) =>{
     res.render('404',{
         title:'404',
@@ -58,6 +95,7 @@ app.get('*',(req,res) =>{
     
     })
 })
+
 
 
 //app.com
