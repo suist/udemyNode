e8341a9fc77d856f6a1be5410313c4ffbba8a895
suist <skoco73@gmail.com>
2020-02-05 09:24:40 +0900

    upload avatar

--- 9-mongoose/src/routers/user.js
+++ 9-mongoose/src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const multer = require('multer')
 const router = new express.Router()
 const auth = require('../middleware/auth')
 const User =require('../models/user')
@@ -171,6 +172,17 @@ router.delete('/users/me',auth, async (req,res)=>{
         
     }
 })
+
+//upload avatar
+
+const upload =multer({
+    dest:'avatars'
+})
+
+router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
+    res.send()
+})
+
 
 
 
