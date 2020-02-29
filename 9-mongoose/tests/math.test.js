6e67dd1b437e22d7930a542d8a9b670cd6f998f6
suist <skoco73@gmail.com>
2020-02-29 21:54:24 +0900

    npm test

---
+++ 9-mongoose/tests/math.test.js
@@ -0,0 +1,18 @@
+test('HelloWorld!',() => {
+
+})
+
+test('This should fail', () => {
+  throw new Error('Failure!')
+})
+
+
+//Why test?
+// Saves time!
+// Create reliable software
+// Gives flexibility to developers
+// Gives flexibility to developers
+// Refactoring
+// collaborating
+// Profiling
+// Peace of mind 
